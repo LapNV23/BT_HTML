6ee2036d06fe6b77aa5088d8849f529ba1bfacf6
LapNV23 <73330679+LapNV23@users.noreply.github.com>
2021-01-21 21:54:30 +0700

    Task3

---
+++ Task3/SQLQuery3.sql
@@ -0,0 +1,80 @@
+﻿IF EXISTS (SELECT * FROM sys.databases WHERE Name='Example5')
+	DROP DATABASE Example5
+GO
+CREATE DATABASE Example5
+GO
+USE Example5
+GO
+--Tạo bảng Lớp học
+CREATE TABLE LopHoc(
+	MaLopHoc INT PRIMARY KEY IDENTITY,
+	TenLopHoc VARCHAR(10)
+)
+GO
+--Tạo bảng Sinh Viên có khóa học là cột MaLopHoc, nối với bảng LopHoc
+CREATE TABLE SinhVien(
+	MaSV int PRIMARY KEY,
+	TenSV varchar(40),
+	MaLopHoc int,
+	CONSTRAINT fk FOREIGN KEY (MaLopHoc) REFERENCES LopHoc (MaLopHoc)
+)
+GO
+--Tạo bảng SanPham với 1 cột NULL, một cột NOT NULL
+CREATE TABLE SanPham (
+	MaSP int NOT NULL,
+	TenSP varchar(40) NULL
+)
+--Tạo bảng với thuộc tính defaullt cho cột Price
+CREATE TABLE StoreProduct(
+	ProductID int NOT NULL,
+	Name varchar(40) NOT NULL,
+	Price money NOT NULL DEFAULT (100)
+)
+--Thử kiểm tra giá trị default có được sử dụng hay không
+INSERT INTO StoreProduct (ProductID, Name) VALUES (111, Rivets)
+GO
+--Tạo bảng ContactPhone với thộc tính IDENTITY
+CREATE TABLE ContactPhone (
+	Person_ID int IDENTITY(500, 1) NOT NULL,
+	MobileNumber bigint NOT NULL
+)
+GO
+--Tạo cột nhận dạng duy nhất tổng thể
+CREATE TABLE CellularPhone (
+	Person_ID uniqueidentifier DEFAULT NEWID() NOT NULL,
+	PersonName varchar(60) NOT NULL
+)
+--Chèn một record vào
+INSERT INTO CellularPhone(PersonName) VALUES('William Smith')
+GO
+--Kiểm tra giá trị của cột Person_ID tự động sinh
+SELECT * FROM CellularPhone
+GO
+--Tạo bảng ContactPhone với cột MobileNumber có thuộc tính UNIQUE
+CREATE TABLE ContactPhone (
+	Person_ID int PRIMARY KEY,
+	MobileNumber bigint UNIQUE,
+	ServiceProvider varchar(30),
+	LandlineNumber bigint UNIQUE
+)
+--Chèn 2 bản ghi có giá trị giống nhau ở cột MobileNumber và LandlieNumber để quan sát lỗi
+INSERT INTO ContactPhone values (101, 983345674, 'Hutch', NULL)
+INSERT INTO ContactPhone values (101, 983345674, 'Alex', NULL)
+GO
+--Tạo bảng PhoneExpeses có một CHECT ở mỗi cột Amount
+CREATE TABLE PhoneExpenses(
+	Expenses_ID int PRIMARY KEY,
+	MobileNumber bigint FOREIGN KEY REFERENCES ContactPhone(MobileNumber)
+	Amount bigint CHECK (Amount >0)
+)
+GO
+--Chỉnh sửa cột trong bảng
+ALTER TABLE ContactPhone
+	ALTER COLUMN ServiceProvider
+GO
+--Thêm một ràng buộc vào bảng
+ALTER TABLE ContactPhone ADD CONSTRAINT CHK_RC CHECK(RentalCharges >0)
+GO
+--Xóa một ràng buộc
+ALTER TABLE Person ContactPhone
+	DROP CONSTRAINT CHK_RC
---
+++ Task3/SQLQuery4.sql
@@ -0,0 +1,51 @@
+﻿create table BookLibary(
+BookCode int primary key,
+BookTitle varchar(100),
+Author varchar(50),
+Edition int,
+BookPrice money,
+Copies int,
+)
+go
+select * from BookLibary
+go
+--tạo insert
+insert into BookLibary values(101,'Hello My Friend','Meis',2020,$1200,5)
+insert into BookLibary values(302,'Hello', 'Mei',2020,$120,5)
+insert into BookLibary values(401,'My','Mes',2019,$2000,8)
+go
+--Tạo bảng Member
+create table Member(
+	MemberCode int primary key,
+	Name varchar(50),
+	Address varchar(100),
+	PhoneNumber int,
+)
+GO
+--Tạo insert 
+insert into Member values(011,'Nguyen','Mieu Dam',0398408820)
+insert into Member values(022,'Van','My Dinh',1234567890)
+insert into Member values(033,'Lap','Me Tri',0987654321)
+GO
+SELECT * from Member
+GO
+
+--Tạo bảng danh sách mượn
+create table IssueDetails(
+	BookCode int,
+	MemberCode int,
+	IssueDate datetime,
+	ReturnDate datetime,
+	constraint ml foreign key (BookCode) references BookLibary(BookCode),
+	constraint mk foreign key (MemBerCode) references Member(MemBerCode),
+)
+GO
+
+
+--tao insert 
+insert into IssueDetails values (2012/12/12,2012/12/12,101,202)
+insert into IssueDetails values (2012/12/12,2012/12/12,302,451)
+insert into IssueDetails values (2012/12/12,2012/12/12,150,102)
+GO
+select * from IssueDetails
+GO
